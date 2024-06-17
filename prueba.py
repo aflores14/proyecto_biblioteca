b7d49b243dc9fba4456aeb87e43bc2f23d87c705
Alejandro Flores <alesjuy@gmail.com>
2024-06-17 18:43:47 -0300

    prueba json

--- prueba.py
+++ prueba.py
@@ -1,1 +1,19 @@
-#prueba 
+#crea alta json para socio.json
+
+import json
+import os
+import time
+
+def alta_socio():
+    #abre el archivo
+    archivo = open("socio.json", "r")
+    #lee el archivo
+    datos = json.load(archivo)
+    #cierra el archivo
+    archivo.close()
+    #abre el archivo
+    archivo = open("socio.json", "w")
+    #lee el archivo
+    datos = json.load(archivo)
+    #cierra el archivo
+    archivo.close()

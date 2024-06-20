3afa6763fbedc6c944229fd9c0c4ffa613a73b05
Alejandro Flores <alesjuy@gmail.com>
2024-06-20 12:00:41 -0300

    cambios en alta usocio

--- socio.py
+++ socio.py
@@ -28,6 +28,31 @@ def escribir_archivo(ruta, datos):
     return True
 
 
+# baja socio: modifica el estado de un socio, 1 para activo , 0 para incactivo y -1 para suspendido
+def modificarEstado(ruta, id_socio,estado):
+    personas = abrir_archivo(ruta)
+    for persona in personas:
+        if persona['id_socio'] == id_socio:
+            persona['estado'] = estado
+
+
+
+# modificar socio, cambia los campos de una persona segun el id_socio
+# def modificarSocio(ruta, id_socio):
+#     personas = abrir_archivo(ruta)
+#     for persona in personas:
+#         if persona['id_socio'] == id_socio:
+#             persona['nombre'] = input("Ingresar el nombre: ")
+#             persona['apellido'] = input("Ingresar el apellido: ")
+#             persona['dni'] = input("Ingresar el dni: ")
+#             persona['fecha_nacimiento'] = input("Ingresar la fecha de nacimiento: ")
+#             persona['direccion'] = input("Ingresar la direccion: ")
+#             persona['telefono'] = input("Ingresar el telefono: ")
+#             persona['email'] = input("Ingresar el email: ")
+#             persona['estado'] = input("Ingresar el estado: ")
+#             persona['fecha_alta'] = input("Ingresar la fecha de alta: ")
+
+
 #registrar campo nuevo en socios.json con los datos
 
 def registrar_persona(ruta, persona):
@@ -47,16 +72,16 @@ def solicitar_fecha_separada():
             # Intentar crear una fecha con los valores ingresados
             fecha_validada = datetime(año, mes, dia)
             print("Fecha válida:", fecha_validada.strftime("%d/%m/%Y"))
-            return fecha_validada
+            return fecha_validada.strftime("%d/%m/%Y")
         except ValueError as e:
             print("Fecha inválida:", e)
             print("Por favor, intenta de nuevo con valores correctos.")
-#registrar campo nuevo en socios.json con los datos
 
+#registrar campo nuevo en socios.json con los datos
 def modificar_persona(ruta, persona):
     personas = abrir_archivo(ruta)
     personas.append(persona)
-    archivo = open(ruta, 'w', encoding='utf-8')
+    archivo = open(ruta, 'w', encoding='UTF-8')
     archivo.write(json.dumps(personas))
 
 
@@ -75,17 +100,11 @@ def listar_personas(ruta):
     for persona in personas:
         print(persona)
 
-#registrar nuevo socio con datos  "nombre": "Carlos",
-        # "apellido":
-        # "fecha_nacimiento": 
-        # "direccion": 
-        # "correo_electronico": 
-        # "telefono": 
-        # "estado": 
+#registrar nuevo socio
 
 def registrar_socio(ruta):
     personas = abrir_archivo(ruta)
-    id_socio = str(int(ultimo_codigo(ruta)) + 1)
+    id_socio = int(ultimo_codigo(ruta)) + 1
     nombre = input("Ingrese nombre: ")
     apellido = input("Ingrese apellido: ")
     fecha_nacimiento = str(solicitar_fecha_separada())
@@ -103,7 +122,6 @@ def registrar_socio(ruta):
         "telefono": telefono,
         "estado": "1"
     }
-    # print(persona)
     personas.append(persona)
     escribir_archivo(ruta,personas)
 
@@ -115,5 +133,3 @@ def registrar_socio(ruta):
 ruta = './proyecto_biblioteca/src/socios.json'
 registrar_socio(ruta)
 listar_personas(ruta)
-# print(ultimo_codigo(ruta))
-# print("ultimo codigo de socios: " , ultimo_codigo(ruta))
